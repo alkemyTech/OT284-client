b2eac43ec969b96348ef7fa3b0df90637e65f904
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-08-22 09:14:59 -0300

    add form with some styles

--- src/app/features/pages/backoffice/backoffice.module.ts
+++ src/app/features/pages/backoffice/backoffice.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
+import { EditHomeComponent } from './edit-home/edit-home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    BackofficeComponent
+    BackofficeComponent,
+    EditHomeComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     BackofficeRoutingModule
   ]
 })
