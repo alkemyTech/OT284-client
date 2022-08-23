47b5a016c799a6fe21fe593525d9c5504a9171dd
FlorÑonquepan <florencianionquepan@outlook.com>
2022-08-23 15:27:11 -0300

    Se agrega componente NewsForm, Material y CKEditor al modulo backoffice

--- src/app/features/pages/backoffice/backoffice-routing.module.ts
+++ src/app/features/pages/backoffice/backoffice-routing.module.ts
@@ -1,12 +1,25 @@
 import { BackofficeComponent } from './backoffice.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsFormComponent } from '../news/news-form/news-form.component';
+//import { GuardGuard } from './services/guard.guard'; FALTA CREARLO
 
 const routes: Routes = [
   { 
     path: "", 
     component: BackofficeComponent
   },
+  {
+    path:"news/:id",
+    component: NewsFormComponent,
+    //canActivate:[GuardGuard]
+  },
+  {
+    path:"news",
+    component: NewsFormComponent,
+    //canActivate:[GuardGuard]
+  },
+
 ];
 
 @NgModule({
--- src/app/features/pages/backoffice/backoffice.module.ts
+++ src/app/features/pages/backoffice/backoffice.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
-
+import { NewsFormComponent } from '../news/news-form/news-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
-    BackofficeComponent
+    BackofficeComponent,
+    NewsFormComponent,
+  ],
+  exports:[
+    NewsFormComponent,
   ],
   imports: [
     CommonModule,
-    BackofficeRoutingModule
+    BackofficeRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    CKEditorModule
   ]
 })
+
 export class BackofficeModule { }
