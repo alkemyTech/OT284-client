f3e96a8584959c6372835e2eb938ac9b1a3d9e2c
FlorÑonquepan <florencianionquepan@outlook.com>
2022-08-23 15:35:33 -0300

    Se elimina Componente NewsForm del features

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -6,7 +6,6 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
 import { CategoriesFormComponent } from "./pages/categories/categories-form/categories-form.component";
-import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
@@ -25,7 +24,6 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     LoginFormComponent,
     RegisterFormComponent,
     CategoriesFormComponent,
-    NewsFormComponent,
     SlidesFormComponent,
     TestimonialFormComponent,
     UserFormComponent,
@@ -38,7 +36,6 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     LoginFormComponent,
     RegisterFormComponent,
     CategoriesFormComponent,
-    NewsFormComponent,
     SlidesFormComponent,
     TestimonialFormComponent,
     UserFormComponent,
