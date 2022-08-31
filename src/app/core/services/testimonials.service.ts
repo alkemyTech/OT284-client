291eea09379dfb6ae63751e9e7e319c4a1787811
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-08-31 15:18:36 -0300

    add testimonial service and interface

---
+++ src/app/core/services/testimonials.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Testimonial } from 'src/app/shared/interfaces/testimonial';
+import { environment } from 'src/environments/environment';
+import { HttpService } from './http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestimonialsService {
+
+  constructor(private http: HttpService) { }
+
+  getTestimonials() {
+    return this.http.get<any>(environment.endpoints.testimonios.list);
+  }
+
+  postTestimonial(testimonio: Testimonial): Observable<any> {
+    return this.http.post(environment.endpoints.testimonios.create, testimonio)
+    //Name y image obligatorios
+  }
+
+}
