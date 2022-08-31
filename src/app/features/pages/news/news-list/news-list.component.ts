53054b3597919daa239ea546e1f08424e3e3c80b
FlorÑonquepan <florencianionquepan@outlook.com>
2022-08-31 11:59:57 -0300

    Se agregan funcionalidades de eliminar y modificar

--- src/app/features/pages/news/news-list/news-list.component.ts
+++ src/app/features/pages/news/news-list/news-list.component.ts
@@ -1,5 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { newData } from '../models/newM';
 import { NewsService } from '../news.service';
 
@@ -11,10 +13,10 @@ import { NewsService } from '../news.service';
 export class NewsListComponent implements OnInit {
   public newsList:newData[]=[]
   public linkCrear:string='/backoffice/news/create';
-  public linkReference: string='CREAR NOVEDAD'
-  constructor(private srcNews:NewsService) { }
-
+  public linkReference: string='CREAR NOVEDAD';
   displayedColumns: string[] = ['demo-image', 'demo-name', 'demo-date', 'demo-delete', 'demo-modify'];
+  
+  constructor(private srcNews:NewsService, private ruta:Router) { }
 
   ngOnInit(): void {
     this.verNovedades();
@@ -32,12 +34,33 @@ export class NewsListComponent implements OnInit {
     })
   }
 
-  public eliminar(id:number):void{
-
+  public eliminar(news:newData):void{
+    Swal.fire({
+      title: `Esta seguro que quiere eliminar la novedad ${news.name}?`,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+      denyButtonText: `Don't delete`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.srcNews.deleteNew(news.id).subscribe({
+          next:(Response:any)=>{
+            //console.log(Response);
+            Swal.fire('Deleted!', '', 'success')
+          },
+          error:(error:HttpErrorResponse)=>{
+            console.log(error.message);
+          }
+        })
+      } else if (result.isDenied) {
+        Swal.fire('The dish was not deleted', '', 'info')
+      }
+    })
+    
   }
 
   public modificar(id:number):void{
-
+    this.ruta.navigate([`backoffice/news/${id}`]);
   }
 
 }
--- src/app/features/pages/news/news.service.ts
+++ src/app/features/pages/news/news.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators';
 import { newData, newM } from './models/newM';
@@ -20,6 +19,10 @@ export class NewsService {
   public getNewModel(id:number):Observable<newData>{
     return this.http.get<any>(`${this.api}/${id}`)
     .pipe(map((result)=>result.data));
+  }
+
+  public deleteNew(id:number):Observable<any>{
+    return this.http.delete<any>(`${this.api}/${id}`);
   }
 
 }
