48dce361ea021df3c284db10c62f90c481b0fed4
FlorÑonquepan <florencianionquepan@outlook.com>
2022-08-31 14:23:59 -0300

    Se modifican los métodos en servicios para que apunten a los endpoints creados

--- src/app/features/pages/news/news.service.ts
+++ src/app/features/pages/news/news.service.ts
@@ -4,21 +4,21 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators';
 import { newData, newM } from './models/newM';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-  private api='https://ongapi.alkemy.org/api/news';
   constructor(private http:HttpClient) { }
 
   public verNews():Observable<newData[]>{
-    return this.http.get<newM>(this.api)
+    return this.http.get<newM>(environment.endpoints.novedades.list)
     .pipe(map((result)=>result.data));
   }
 
   public getNewModel(id:number):Observable<newData>{
-    return this.http.get<any>(`${this.api}/${id}`)
+    return this.http.get<any>(environment.endpoints.novedades.detail(id))
     .pipe(map((result)=>result.data));
   }
 
