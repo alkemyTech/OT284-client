bc6ed167d8de9b8fddd214c929117636ca998bfd
unknown <emilioemoran@gmail.com>
2022-08-31 16:31:30 -0300

    error handling added

--- src/app/shared/components/map-window/map-window.component.ts
+++ src/app/shared/components/map-window/map-window.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import * as L from 'leaflet';
-import { latLng, tileLayer } from 'leaflet';
+import { FormMapService } from 'src/app/features/pages/backoffice/users/form-create-user/form-map/form-map.service';
 
 @Component({
   selector: 'app-map-window',
@@ -8,23 +9,42 @@ import { latLng, tileLayer } from 'leaflet';
   styleUrls: ['./map-window.component.scss']
 })
 export class MapWindowComponent implements OnInit {
-/* 
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' })
-    ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
-  }; */
 
-  constructor() { }
+  lat: number;
+  long:number;
+  @Input() address: string;
 
-  ngOnInit(): void {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 10,
-      attribution: '© OpenStreetMap'
-  }).addTo(map);
+ 
+  constructor(private formMap: FormMapService, public dialog: MatDialog) { }
+
+  async ngOnInit(){
+    this.formMap.mapAddress = this.address
+    this.formMap.getCoordinates().subscribe(
+      (data: any) => {
+        this.lat =data.results[0].position.lat;
+        this.long = data.results[0].position.lon;
+        var map = L.map('map').setView([this.lat, this.long], 17);
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: '© OpenStreetMap'
+          }).addTo(map);
+            this.addMarker(this.lat, this.long , map)
+          },
+      (error: any) => {
+        this.dialog.open(MatAlertErrorComponent , {
+          data: {text:`error al cargar el mapa `, message: error},
+        })
+      }
+    )
+  }
+
+  addMarker(lat:number, long:number, map: L.Map){
+    var myIcon = L.icon({
+      iconUrl: 'http://ongapi.alkemy.org/storage/oqhHt6tOMb.png',
+      iconSize: [38, 60],
+      });
+    var marker = L.marker([lat, long], {icon: myIcon}).addTo(map)
+		.bindPopup('<b>Hello everybody!</b><br />Here is our ONG.').openPopup();
   }
 
 }
