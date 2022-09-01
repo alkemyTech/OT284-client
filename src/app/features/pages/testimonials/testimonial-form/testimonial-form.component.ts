a2becf755c66fe78b3de89719b91bd9bd318b03f
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-09-01 08:54:28 -0300

    add post to testimonials

--- src/app/features/pages/testimonials/testimonial-form/testimonial-form.component.ts
+++ src/app/features/pages/testimonials/testimonial-form/testimonial-form.component.ts
@@ -11,6 +11,7 @@ import { Testimonial } from 'src/app/shared/interfaces/testimonial';
 export class TestimonialFormComponent implements OnInit {
 
   public formulario!: FormGroup;
+  imgBase64!: any;
   @Input() element: any = {}
 
   constructor(private fb: FormBuilder, private service: TestimonialsService) {
@@ -22,6 +23,58 @@ export class TestimonialFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.listadoTestimonios();
+  }
+
+  async enviar() {
+    console.info('FORMULARIO', this.formulario);
+    console.log(this.formulario.valid);
+
+    if (this.formulario.valid) {
+      //ENVIAR
+      let testimonio: Testimonial;
+      setTimeout(() => {
+
+        testimonio = {
+          name: 'Prueba Testimonio',
+          id: 199,
+          image: this.imgBase64,
+          description: '',
+          created_at: new Date(Date.now()),
+          deleted_at: new Date(2022, 2, 15),
+          updated_at: new Date(Date.now()),
+        }
+
+        if (Object.entries(this.element).length == 0) {
+          //Agregar -> POST
+          this.service.postTestimonial(testimonio).subscribe((data: any) => {
+            if (data.error) {
+              console.info('HUBO UN ERROR: ', data.error);
+            }
+            else {
+              console.info("EXITO", data);
+            }
+          })
+        }
+        else {
+          //Modificar -> PUT
+        }
+      }, 1000);
+
+    }
+    else {
+      //ERROR
+    }
+  }
+
+  async fileEvent(e: Event) {
+    let imagen = this.formulario.controls.img.value;
+    // console.log(imagen);
+    this.convertFileToBase64(imagen);
+  }
+
+
+  listadoTestimonios() {
     this.service.getTestimonials().subscribe((data: any) => {
 
       if (data.success) {
@@ -33,14 +86,15 @@ export class TestimonialFormComponent implements OnInit {
     })
   }
 
-  enviar() {
-    console.info('FORMULARIO', this.formulario);
-    console.log(this.formulario.valid);
-
-    if (this.formulario.valid) {
-      //ENVIAR
-    }
+  async convertFileToBase64(file: any) {
+    const reader = new FileReader();
+    let resultado;
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      resultado = reader.result
+      this.imgBase64 = reader.result?.toString();
+      console.log(this.imgBase64);
+    };
   }
-
 
 }
