adf09cee372e0f747d761c01c1d5133b3bd3d7a0
Maximiliano Guaymas <maxikasta480@gmail.com>
2022-09-02 14:51:51 -0300

    create news.members controller

---
+++ src/app/core/services/news-members.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsMembersService {
+
+  constructor(private httpService:HttpService) { }
+
+  getMembers(){
+
+  }
+
+  postMembers(){
+
+  }
+
+  getMember(){
+
+  }
+
+  putMember(){
+
+  }
+
+  deleteMember(){
+
+  }
+}
