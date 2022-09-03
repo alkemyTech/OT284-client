55755212cfe90728e92f40d38cebb9c7b185a713
Maximiliano Guaymas <maxikasta480@gmail.com>
2022-09-03 18:48:29 -0300

    métodos necesarios para realizar las peticiones de los componentes de Miembros a la API

--- src/app/core/services/news-members.service.ts
+++ src/app/core/services/news-members.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsMembersService {
+  url: string = environment.url + 'members'; //hasta crear variables de entorno correspondientes
 
   constructor(private httpService:HttpService) { }
 
-  getMembers(){
-
+  getMembers(searchQuery ?: string, skipQuery?: string, limitQuery?: string){
+    return this.httpService.get(this.url);
   }
 
-  postMembers(){
-
+  postMembers(body: any){
+    return this.httpService.post(this.url, body);
   }
 
-  getMember(){
-
+  getMember(id: number){
+    return this.httpService.get(this.url.concat(`/${id}`));
   }
 
-  putMember(){
-
+  putMember(id:number, body: any){
+    return this.httpService.put(this.url.concat(`/${id}`),body);
   }
 
-  deleteMember(){
-
+  deleteMember(id: number){
+    return this.httpService.delete(this.url.concat(`/${id}`));
   }
 }
