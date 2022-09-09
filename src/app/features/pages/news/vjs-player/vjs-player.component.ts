a8e2c8c37042a9db39709f6d60ebd3f0a86294da
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-09 11:32:01 -0300

    actualizo ts del componente

--- src/app/features/pages/news/vjs-player/vjs-player.component.ts
+++ src/app/features/pages/news/vjs-player/vjs-player.component.ts
@@ -1,7 +1,6 @@
-import { OnDestroy, ViewEncapsulation, ElementRef,Input,ViewChild, AfterViewInit } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy, ViewEncapsulation, ElementRef,Input,ViewChild, AfterViewInit } from '@angular/core';
 import videojs from 'video.js';
-
+import 'videojs-youtube';
 
 @Component({
   selector: 'app-vjs-player',
@@ -10,7 +9,7 @@ import videojs from 'video.js';
   encapsulation: ViewEncapsulation.None,
 })
 export class VjsPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
-  @ViewChild('target', {static: true}) target: ElementRef;
+  @ViewChild('target') target: ElementRef;
 
   @Input() options: {
     fluid: boolean,
@@ -19,33 +18,23 @@ export class VjsPlayerComponent implements OnInit, OnDestroy, AfterViewInit {
     sources: {
       src: string,
       type: string,
-    }[],
+      }[],
   };
   player:videojs.Player;
-
+  
   constructor(private elementRef:ElementRef) {
     
    }
 
   ngOnInit() {
-    let body=document.body;
-    let script=document.createElement('script');
-    script.type='text/javascript';
-    script.innerHTML='';
-    script.src='./youtube.js';
-    script.async=true;
-    script.defer=true;
-    body.appendChild(script);
+
   }
 
   ngAfterViewInit() {
     this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
       console.log('onPlayerReady', this);
-    });
-    this.player.canPlayType('videoyoutube')
-    this.player.src('https://www.youtube.com/watch?v=4YnSk1gI_Oo');
+      });
   }
-
 
   ngOnDestroy() {
     if (this.player) {
