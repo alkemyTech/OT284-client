47345b49d411120e3780be7b3194468db855c9c4
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-09 19:23:01 -0300

    Se crean actions para renderizar el listado de novedades

---
+++ src/app/state/actions/news.action.ts
@@ -0,0 +1,11 @@
+import { createAction,props } from "@ngrx/store";
+import { newData } from "src/app/features/pages/news/models/newM";
+
+export const loadNews=createAction(
+    '[News List] Load News'
+);
+
+export const loaderNews=createAction(
+    '[News List] Loaded News',
+    props<{news:newData[]}>()
+)
