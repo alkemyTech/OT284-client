a8bd6ccea3152a05b3c953e455d444cba2f3092d
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-10 17:57:23 -0300

    Se agrega news en store

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,8 +1,10 @@
 import { ActionReducerMap } from "@ngrx/store";
 import { memberState } from "../shared/interfaces/members.state";
+import { NewsState } from "../shared/interfaces/news.state";
 import { OrganizationState } from "../shared/interfaces/organization.state";
 import { userState } from "../shared/interfaces/users.state";
 import { membersReducer } from "./reducers/members.reducer";
+import { newsReducer } from "./reducers/news.reducer";
 import { organizationReducer } from "./reducers/organization.reducer";
 import { usersReducer } from "./reducers/users.reducer";
 
@@ -10,10 +12,12 @@ export interface AppState {
   organization: OrganizationState;
   users: userState;
   members: memberState;
+  news: NewsState;
 }
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
   organization: organizationReducer,
   users: usersReducer,
-  members: membersReducer
+  members: membersReducer,
+  news: newsReducer
 };
