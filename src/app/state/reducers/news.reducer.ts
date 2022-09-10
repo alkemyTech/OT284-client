7c29dd79c58a6d20adf400b7e9f86c37049fecec
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-10 19:57:46 -0300

    Se agrega la acción del error al reducer

--- src/app/state/reducers/news.reducer.ts
+++ src/app/state/reducers/news.reducer.ts
@@ -1,16 +1,19 @@
 import {createReducer, on} from '@ngrx/store';
 import { newData } from 'src/app/features/pages/news/models/newM';
 import { NewsState } from 'src/app/shared/interfaces/news.state';
-import { loadedNews, loadNews } from '../actions/news.action';
+import { errorLoadedNews, loadedNews, loadNews } from '../actions/news.action';
 
-export const initialState: NewsState = {loading:false, news:[]}
+export const initialState: NewsState = {loading:false, news:[], error:''}
 
 export const newsReducer= createReducer(
     initialState,
     on(loadNews,(state)=>{
-        return {...state, loading:true}
+        return {...state, loading:true, error:''}
     }),
     on(loadedNews,(state, {news})=>{
         return {...state,loading:false, news}
+    }),
+    on(errorLoadedNews,(state, {message})=>{
+        return {...state, loading:false, error:message}
     })
 )
