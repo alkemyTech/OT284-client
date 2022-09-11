e8f7c002d9acd6c4b93cbe775075e4a7645817c1
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-10 22:20:13 -0300

    Se agrega el efecto de error al cargar news

--- src/app/state/effects/news.effects.ts
+++ src/app/state/effects/news.effects.ts
@@ -1,8 +1,11 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import {Actions, createEffect, ofType} from "@ngrx/effects"
-import { EMPTY } from "rxjs/internal/observable/empty";
-import { mergeMap, map, catchError } from "rxjs/operators";
+import { of } from "rxjs";
+import { mergeMap, map, catchError, tap } from "rxjs/operators";
 import { NewsService } from "src/app/features/pages/news/news.service";
+import { MatAlertDialogComponent } from "src/app/shared/components/mat-alert-dialog/mat-alert-dialog.component";
 
 @Injectable()
 export class NewsEffects{
@@ -11,13 +14,25 @@ export class NewsEffects{
         ofType('[News List] Load News'),
         mergeMap(()=>this.srcNews.verNews()
         .pipe(
-            map(news=>({type:'[News List] Loaded News', news})),
-            catchError(()=>EMPTY) 
-        ))
+            map((news)=>({type:'[News List] Loaded News', news})),
+            catchError((error:HttpErrorResponse) => of({ type: '[News List] Not Loaded News', message: error.message}))
+            )
+        )
         )
     )
+
+    errorNews$=createEffect(()=>this.actions$.pipe(
+        ofType('[News List] Not Loaded News'),
+        tap(()=>this.dialog.open(MatAlertDialogComponent,{
+            data:{text:'Error al cargar novedades', message: 'Error de conexión al cargar novedades'},
+        }))
+    ),
+        {dispatch:false}
+    )
+
     constructor(
         private actions$:Actions,
-        private srcNews:NewsService
+        private srcNews:NewsService,
+        public dialog: MatDialog
     ){}
 }
