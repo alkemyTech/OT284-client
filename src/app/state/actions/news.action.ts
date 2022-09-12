e51ecaed83df5d01564b6a12fc0a50f224a2a6f5
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-12 14:36:04 -0300

    Se modifica notación de acciones para cargar novedades

--- src/app/state/actions/news.action.ts
+++ src/app/state/actions/news.action.ts
@@ -1,23 +1,30 @@
 import { createAction,props } from "@ngrx/store";
 import { newData } from "src/app/features/pages/news/models/newM";
 
+export const NEWS_LOAD='[News List] Load News';
+export const NEWS_LOADED= '[News List] Loaded News';
+export const NEWS_LOAD_FAILURE='[News List] Not Loaded News';
+
 export const NEW_DELETE_ALERT='[News List] Alert Delete New';
 export const NEW_NOT_DELETE='[News List] Not Deleted New'
 export const NEW_DELETE='[News List] Delete New';
 export const NEW_DELETED='[News List] Deleted New';
 export const NEW_DELETE_FAILURE='[News List] Error delete New';
+
 export const loadNews=createAction(
-    '[News List] Load News'
+    NEWS_LOAD
 );
 
 export const loadedNews=createAction(
-    '[News List] Loaded News',
+    NEWS_LOADED,
     props<{news:newData[]}>()
 )
 
 export const errorLoadedNews=createAction(
-    '[News List] Not Loaded News',
+    NEWS_LOAD_FAILURE,
     props<{message:string}>()
+)
+
 export const alertDelete=createAction(
     NEW_DELETE_ALERT,
     props<{newToDelete:newData}>()
