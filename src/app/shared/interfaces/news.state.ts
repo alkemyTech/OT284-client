502060580d1b62a085151e260688aabbb67f7d0d
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-12 14:59:54 -0300

    Se agrega propiedad newDeleted a interface NewsState

--- src/app/shared/interfaces/news.state.ts
+++ src/app/shared/interfaces/news.state.ts
@@ -3,5 +3,6 @@ import { newData } from "src/app/features/pages/news/models/newM";
 export interface NewsState{
     loading:boolean,
     news: newData[],
-    error:string
+    error:string,
+    newDeleted:boolean,
 }
