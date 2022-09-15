14d57a5eb470ba63096b00cd5d7a40968d1757d9
FlorÑonquepan <florencianionquepan@outlook.com>
2022-09-15 08:28:10 -0300

    se actualiza el reducer

--- src/app/state/reducers/news.reducer.ts
+++ src/app/state/reducers/news.reducer.ts
@@ -1,13 +1,15 @@
 import {createReducer, on} from '@ngrx/store';
 import { newData } from 'src/app/features/pages/news/models/newM';
 import { NewsState } from 'src/app/shared/interfaces/news.state';
-import { deletedNew, errorLoadedNews, loadedNews, loadNews, receivedNew } from '../actions/news.action';
+import { deletedNew, deleteNew, errorLoadedNews, loadedNews, loadNews, receivedNew } from '../actions/news.action';
 
-export const initialND: newData={
+export const initialNew: newData={
     id: 0, name: "", slug: null, content: "", image: "", user_id: 0, category_id: 0,
-    created_at: "", updated_at: "", deleted_at: null, group_id: null
+    created_at: "", updated_at: "", deleted_at:'', group_id: null
 }
-export const initialState: NewsState = {loading:false, news:[], error:'', newDeleted:false, newToEdit: initialND}
+
+export const initialState: NewsState = {loading:false, news:[], error:'', 
+                                        newWasDeleted:false, newToDelete:initialNew, newToEdit: initialNew, newWasCreated:false}
 
 export const newsReducer= createReducer(
     initialState,
@@ -20,10 +22,27 @@ export const newsReducer= createReducer(
     on(errorLoadedNews,(state, {message})=>{
         return {...state, loading:false, error:message}
     }),
-    on(deletedNew, (state,{newDeleted})=>{
-        return {...state, news: state.news.filter(newM=>newM.id!==newDeleted.id), newDeleted:true}
+    on(deleteNew,(state,{newToDelete})=>{
+        return {...state, newToDelete}
+    }),
+    on(deletedNew, (state)=>{
+        let newDeleted=state.newToDelete
+        return {...state, news: state.news.filter(newM=>newM.id!==newDeleted.id), newWasDeleted:true}
     }),
     on(receivedNew,(state,{newToEdit})=>{
         return {...state, newToEdit}
+    }),
+/*     on(createdNew,(state, {newCreated})=>{
+        return {...state,news:state.news.push(newCreated), newWasCreated:true}
+    }) */
+)
+
+/* export const formNewsReducer=createReducer(
+    initialFormState,
+    on(formValueChange,(state,{type, ...update})=>{
+        return {...state,...update}
     })
 )
+export function reducer(state: NewsFormState | undefined, action: Action){
+    return formNewsReducer(state,action)
+} */
