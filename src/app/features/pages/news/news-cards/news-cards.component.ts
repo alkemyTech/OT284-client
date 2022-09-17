16e59cabeda970d579fad47a6e49d266e56ecd5e
JuanSanchezKZ <juanzete@gmail.com>
2022-09-17 16:14:22 -0300

    agregue spinner en pantalla de novedades

--- src/app/features/pages/news/news-cards/news-cards.component.ts
+++ src/app/features/pages/news/news-cards/news-cards.component.ts
@@ -1,54 +1,60 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { MatAlertErrorComponent } from 'src/app/shared/components/mat-alert-error/mat-alert-error.component';
-import { newData } from '../models/newM';
-import { NewsService } from '../news.service';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { MatAlertErrorComponent } from "src/app/shared/components/mat-alert-error/mat-alert-error.component";
+import { newData } from "../models/newM";
+import { NewsService } from "../news.service";
 
 @Component({
-  selector: 'app-news-cards',
-  templateUrl: './news-cards.component.html',
-  styleUrls: ['./news-cards.component.scss']
+  selector: "app-news-cards",
+  templateUrl: "./news-cards.component.html",
+  styleUrls: ["./news-cards.component.scss"],
 })
 export class NewsCardsComponent implements OnInit {
-  public newsLista: newData[]=[]
-  public newModel!:newData;
-  public myTitle:string;
-  constructor(private newsService: NewsService, private ruta:Router, public dialog: MatDialog) {
-    this.myTitle="Novedades";
+  public newsLista: newData[] = [];
+  public newModel!: newData;
+  public myTitle: string;
+  public isLoading: boolean;
+  constructor(
+    private newsService: NewsService,
+    private ruta: Router,
+    public dialog: MatDialog
+  ) {
+    this.myTitle = "Novedades";
+    this.isLoading = true;
   }
 
   ngOnInit(): void {
     this.verNovedades();
   }
 
-  public verNovedades():void{
+  public verNovedades(): void {
     this.newsService.verNews().subscribe({
-      next:(Response:newData[])=>{
-        this.newsLista=Response;
+      next: (Response: newData[]) => {
+        this.newsLista = Response;
+        this.isLoading = false;
       },
-      error:(error:HttpErrorResponse)=>{
-        this.dialog.open(MatAlertErrorComponent,{
-          data:{text:"Error al cargar novedades", message:error.message},
-        })
-      }
-    })
+      error: (error: HttpErrorResponse) => {
+        this.dialog.open(MatAlertErrorComponent, {
+          data: { text: "Error al cargar novedades", message: error.message },
+        });
+      },
+    });
   }
 
-  public mostrarFechaAct(fechaCreacion:string,fechaAct:string){
-    let result=false;
-    let creationDate=Date.parse(fechaCreacion);
-    let uploadedDate=Date.parse(fechaAct);
+  public mostrarFechaAct(fechaCreacion: string, fechaAct: string) {
+    let result = false;
+    let creationDate = Date.parse(fechaCreacion);
+    let uploadedDate = Date.parse(fechaAct);
     //console.log("CReacion:"+fechaCreacion+". Actualiz:"+fechaAct);
-    if(creationDate<uploadedDate){
-      result=true;
+    if (creationDate < uploadedDate) {
+      result = true;
     }
     return result;
   }
-  
-  public detalleVer(novedad:newData){
+
+  public detalleVer(novedad: newData) {
     this.ruta.navigate([`novedades/${novedad.id}`]);
   }
-
 }
