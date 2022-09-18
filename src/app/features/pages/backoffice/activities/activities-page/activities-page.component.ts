e8bd21ab5b172afcac25adf602da7eb176f4df46
unknown <emilioemoran@gmail.com>
2022-09-18 13:53:29 -0300

    list of activities

--- src/app/features/pages/backoffice/activities/activities-page/activities-page.component.ts
+++ src/app/features/pages/backoffice/activities/activities-page/activities-page.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from "@angular/material/table";
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Activity } from 'src/app/shared/interfaces/activity';
+import { loadActivities } from 'src/app/state/actions/activities.actions';
 import { AppState } from 'src/app/state/app.state';
+import { selectActivities } from 'src/app/state/selectors/activities.selector';
 import { activitiesExample } from '../../../activities/activity-view/activities-example';
 
 @Component({
@@ -14,11 +17,13 @@ import { activitiesExample } from '../../../activities/activity-view/activities-
 export class ActivitiesPageComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'image', 'created_at', 'actions'];
-  dataSource : Activity[]= activitiesExample
+  dataSource$ : Observable<Activity[]> 
 
   constructor(private store:Store<AppState>, private route:Router) { }
 
   ngOnInit(): void {
+    this.store.dispatch(loadActivities());
+    this.dataSource$ = this.store.select(selectActivities)
   }
 
   editActivity(activity: Activity){
