249d10f80167d768b1b3f92c1846feb2e567a946
Maximiliano Guaymas <maxikasta480@gmail.com>
2022-09-19 22:17:53 -0300

    testing formulario login

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,16 +1,35 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const authServiceMock = jasmine.createSpyObj('AuthService',{
+    userslastlogin: () => of({}) 
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{provide: AuthService, useValue: authServiceMock}]
+    });
     service = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
+  it('Debe existir el servicio', () => {
     expect(service).toBeTruthy();
   });
+
+  // it('Login Firebase exitoso.', () => {
+
+  //   const mockDataLogin = {email: 'maxi@gmail.com', password: 'Maxiasd123$'};
+
+  //   expect(localStorage.getItem('uid')).toBeTruthy();
+
+  // });
+
+
 });
--- src/app/features/pages/auth/login-form/login-form.component.spec.ts
+++ src/app/features/pages/auth/login-form/login-form.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { RouterTestingModule} from '@angular/router/testing';
 
 import { LoginFormComponent } from './login-form.component';
 
@@ -6,9 +10,18 @@ describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
 
+  const authServiceMock = jasmine.createSpyObj('AuthService',{
+    userslastlogin: () => of({}) 
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
+      imports: [FormsModule,ReactiveFormsModule,RouterTestingModule.withRoutes([])],
+      declarations: [ LoginFormComponent ],
+      providers: [
+        {provide: AuthService, useValue: authServiceMock},
+        
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +32,32 @@ describe('LoginFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Debe existir el componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Formulario invalido, falta ingresar password', () => {
+    const app = fixture.componentInstance;
+    const email = app.loginForm.controls['email'];
+    email.setValue('maxi@gmail.com')
+    expect(app.loginForm.invalid).toBeTruthy();
+  });
+
+  it('Formulario invalido, falta ingresar email', () => {
+    const app = fixture.componentInstance;
+    const password = app.loginForm.controls['password'];
+    password.setValue('Maxiasd123$');
+    expect(app.loginForm.invalid).toBeTruthy();
+  });
+
+  it('Formulario valido, se puede hacer submit', () => {
+    const app = fixture.componentInstance;
+    const email = app.loginForm.controls['email'];
+    email.setValue('maxi@gmail.com')
+    const password = app.loginForm.controls['password'];
+    password.setValue('Maxiasd123$');
+    expect(app.loginForm.valid).toBeTruthy();
+  });
+
+
 });
