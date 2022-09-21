075e084a6997b51c155fe505ceebe8fcefbc9a6c
unknown <emilioemoran@gmail.com>
2022-09-21 18:22:37 -0300

    initial commit

--- src/app/features/pages/contact/contact-form/contact-form.component.spec.ts
+++ src/app/features/pages/contact/contact-form/contact-form.component.spec.ts
@@ -22,4 +22,23 @@ describe('ContactFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  const fillForm = () => {
+    setFieldValue( '', "name");
+    setFieldValue( '', "email");
+    setFieldValue( '', "phone");
+    setFieldValue( '', "message");
+  }
+
+  it('should not submit on form error', () => {
+    fillForm()
+    fixture.debugElement
+   /*  fixture.debugElement.nativeElement.querySelector('#form').triggerEventHandler('submit', {});
+
+    expect(signupService.signup).toThrowError */
+  });
+
+  function setFieldValue(value: string, input: any) {
+    const ctrl = component.contactForm.get(input)
+    ctrl?.setValue(value)
+  }
 });
