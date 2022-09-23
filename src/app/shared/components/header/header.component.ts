7ab5de76074b4c656b00c386eabaa1325c3bab99
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-09-23 11:17:39 -0300

    add tests header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -23,10 +23,91 @@ export class HeaderComponent implements OnInit {
       text: 'Contacto',
       link: 'contacto',
       render: true
-    }]
+    }
+  ]
   constructor(public router: Router) { }
 
   ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem("UserData") || "{}");
+    if (JSON.stringify(user) == "{}") {
+
+      this.list = [
+        {
+          text: 'Inicio',
+          link: '',
+          render: true
+        },
+        {
+          text: 'Nosotros',
+          link: 'nosotros',
+          render: true
+        },
+        {
+          text: 'Contacto',
+          link: 'contacto',
+          render: true
+        }
+      ]
+    } else {
+
+      if (user.tipo == "admin") {
+        this.list = [
+          {
+            text: 'Inicio',
+            link: '',
+            render: true
+          },
+          {
+            text: 'Nosotros',
+            link: 'nosotros',
+            render: true
+          },
+          {
+            text: 'Actividades',
+            link: 'actividades',
+            render: true
+          },
+          {
+            text: 'Novedades',
+            link: 'novedades',
+            render: true
+          },
+          {
+            text: 'Escritorio',
+            link: 'backoffice',
+            render: true
+          }
+        ]
+      } else if (user.tipo == "usuario") {
+        this.list = [
+          {
+            text: 'Inicio',
+            link: '',
+            render: true
+          },
+          {
+            text: 'Nosotros',
+            link: 'nosotros',
+            render: true
+          },
+          {
+            text: 'Actividades',
+            link: 'actividades',
+            render: true
+          },
+          {
+            text: 'Novedades',
+            link: 'novedades',
+            render: true
+          },
+          {
+            text: 'Contacto',
+            link: 'contacto',
+            render: true
+          }
+        ]
+      }
+    }
   }
 
 }
