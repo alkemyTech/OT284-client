60ff98e81b029d50d57533d65c7b63480d3b8101
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-09-23 11:55:04 -0300

    add docs header tests

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -25,6 +25,42 @@ describe('HeaderComponent', () => {
   });
 
 
+  /** Verifica que si un usuario no esta autenticado solo muestre los links publicos
+   *  Partimos de la base de que no haya un usuario guardado en el LocalSortage.
+   *  Cargamos el componente asi se carga la lista de elementos que se debe mostrar.
+   *  Verificamos que en la lista no haya ningun elemento que no se tenga que mostrar.
+   *  Verificamos que en la lista se encuentren todos los elementos que se deberian mostrar.
+   */
+  it('Usuario NO autenticado', () => {
+    let verifica: boolean = true;
+
+    localStorage.removeItem("UserData");
+    component.ngOnInit();
+    component.list.forEach((element: any) => {
+      if (element.text != "Inicio" && element.text != "Contacto" && element.text != "Nosotros")
+        verifica = false
+    });
+
+    let cantidadItems = 0
+    component.list.forEach((element: any) => {
+      if (element.text == "Contacto" || element.text == "Inicio" || element.text == "Nosotros") {
+        cantidadItems++;
+      }
+    });
+    if (cantidadItems < 3) {
+      verifica = false;
+    }
+
+    expect(verifica).toBeTrue();
+  });
+
+
+  /** Verifica que se muestren en el header los elementos correspondientes a un usuario normal logueado.
+   *  Partimos de la base de que haya un usuario normal logueado.
+   *  Cargamos el componente asi se carga la lista de elementos que se debe mostrar.
+   *  Verificamos que en la lista no haya ningun elemento que no se tenga que mostrar.
+   *  Verificamos que en la lista se encuentren todos los elementos que se deberian mostrar.
+   */
   it('Usuario normal autenticado', () => {
     let verifica: boolean = true;
     let userPrueba = {
@@ -52,6 +88,12 @@ describe('HeaderComponent', () => {
     expect(verifica).toBeTrue();
   });
 
+  /** Verifica que se muestren en el header los elementos correspondientes a un usuario admin logueado.
+   *  Partimos de la base de que haya un usuario admin logueado.
+   *  Cargamos el componente asi se carga la lista de elementos que se debe mostrar.
+   *  Verificamos que en la lista no haya ningun elemento que no se tenga que mostrar.
+   *  Verificamos que en la lista se encuentren todos los elementos que se deberian mostrar.
+   */
   it('Usuario Admin autenticado', () => {
     let verifica: boolean = true;
     let userPrueba = {
@@ -73,33 +115,6 @@ describe('HeaderComponent', () => {
       }
     });
     if (cantidadItems < 5) {
-      verifica = false;
-    }
-
-    expect(verifica).toBeTrue();
-  });
-
-
-
-  it('Usuario NO autenticado', () => {
-    let verifica: boolean = true;
-    let userPrueba = {
-    }
-    localStorage.setItem("UserData", JSON.stringify(userPrueba))
-
-    component.ngOnInit();
-    component.list.forEach((element: any) => {
-      if (element.text != "Inicio" && element.text != "Contacto" && element.text != "Nosotros")
-        verifica = false
-    });
-
-    let cantidadItems = 0
-    component.list.forEach((element: any) => {
-      if (element.text == "Contacto" || element.text == "Inicio" || element.text == "Nosotros") {
-        cantidadItems++;
-      }
-    });
-    if (cantidadItems < 3) {
       verifica = false;
     }
 
